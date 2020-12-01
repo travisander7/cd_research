d24921f3b5bb680de87604ecee3c6f913be79ab9
Travis Andersen <andert714@gmail.com>
2020-11-30 17:57:47 -0700

    Reorganized fixed effects from scratch without tidyverse

--- fixed.R
+++ fixed.R
@@ -2,38 +2,23 @@
 #   Run a simple meta analysis and look out output
 #   Do some by hand (Inverse variance fixed effect and DerSimonian Laird random effects)
 #   Find dataset or make up dataset
-library(tidyverse)
 
-df <- read_csv('binary_data.csv')
+df <- readr::read_csv('binary_data.csv')
 m <- meta::metabin(Ee, Ne, Ec, Nc, data = df, studlab = Author, method = 'Inverse', sm = 'OR')
 
 # Adjust for zeros
 df[5, 2:5] <- df[5, 2:5] + 0.5
 
-# Calculations for each study
-by_study <- df %>%
-  mutate(
-    OR = (Ee*(Nc-Ec))/(Ec*(Ne-Ee)),
-    var = 1/Ee + 1/(Nc-Ec) + 1/Ec + 1/(Ne-Ee),
-    lower = exp(qnorm(0.025, log(OR), sqrt(var))),
-    upper = exp(qnorm(0.975, log(OR), sqrt(var))),
-    weight = 1/var,
-    w_perc = 100*weight/sum(weight)
-  )
+# Calculate study statistics
+y <- with(df, (Ee*(Nc-Ec))/(Ec*(Ne-Ee)))
+v <- with(df, 1/Ee + 1/(Nc-Ec) + 1/Ec + 1/(Ne-Ee))
+lower <- exp(qnorm(0.025, log(y), sqrt(v)))
+upper <- exp(qnorm(0.975, log(y), sqrt(v)))
+w <- 1/v
 
-# Pooled calculations
-pooled <- by_study %>%
-  summarize(
-    OR = exp(sum(log(OR)*w_perc)/100),
-    var = 1/sum(weight)
-  ) %>%
-  mutate(
-    lower = exp(qnorm(0.025, log(OR), sqrt(var))),
-    upper = exp(qnorm(0.975, log(OR), sqrt(var))),
-    z = log(OR)/sqrt(var),
-    p_value = 2*pnorm(z)
-  )
-
-print(m)
-print(by_study)
-print(pooled)
+# Pool study statistics
+OR <- exp(sum(w*log(y))/sum(w))
+var <- 1/sum(w)
+ci <- exp(qnorm(c(0.025, 0.975), log(OR), sqrt(var)))
+z <- log(OR)/sqrt(var)
+p_value <- 2*pnorm(z)
