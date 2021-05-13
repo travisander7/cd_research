6b1a1537ff6d56fe0dcbfbc42732e28f43924215
Travis Andersen <andert714@gmail.com>
2021-05-13 11:17:48 -0600

    Changed antibiotics to use get_cd

--- code/antibiotics.R
+++ code/antibiotics.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-source('code/get_est.R')
+source('code/get_cd.R')
 source("zab_code/FullOriginalGmetaCode.R")
 # AntibioticsRheumaticFeverData.csv: data
 # AntibioticsRheumaticFever1.txt: data (long format)
@@ -11,30 +11,13 @@ t_obs <- sum(data$Antibiotics_Event)
 
 dist <- read_table('data/AntibioticsRheumaticFever_distn_32.txt', col_names = c('t', 'p', 'p.norm'))
 
-#################### Exact Permutation method ####################
-cd_df <- exp(seq(-5, 0, length = 100)) %>%
-  set_names %>%
-  map_df(function(alpha) get_est(dist, t_obs, alpha), .id = 'alpha') %>%
-  mutate_at('alpha', as.numeric)
-
-#################### Liu method ####################
-liu <- gmeta(
-  as.matrix(data), 
-  gmi.type = "2x2", 
-  method = "exact1", 
-  gmo.xgrid = seq(-2, 1, length = 1000), 
-  report.error = TRUE
-)
-
-liu_df <- tibble(
-  log_OR = liu$x.grids,
-  p_value = 1 - 2*abs(0.5-liu$combined.cd)
-)
-
 #################### Graph ####################
-ggplot(cd_df) +
-  geom_line(aes(midp_lower, alpha)) +
-  geom_line(aes(midp_upper, alpha)) +
-  geom_line(aes(lower, alpha), linetype = 'dashed') +
-  geom_line(aes(upper, alpha), linetype = 'dashed') +
-  geom_line(aes(log_OR, p_value), liu_df, col = 'blue')
+tibble(
+  theta = seq(-2, 1, length = 1000),
+  perm = 1 - 2*abs(0.5-map_dbl(theta, get_cd, dist, t_obs)),
+  liu = 1 - 2*abs(0.5-gmeta(as.matrix(data), gmi.type = '2x2', method = 'exact1', gmo.xgrid = theta, report.error = TRUE)$combined.cd)
+) %>%
+  pivot_longer(-theta, names_to = 'method', values_to = 'alpha') %>%
+  ggplot(aes(theta, alpha, col = method)) +
+  geom_line() +
+  geom_hline(yintercept = 0.05, linetype = 'dashed')
