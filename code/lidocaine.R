3929416c596b2798624cad7b0e049963b379c402
Travis Andersen <andert714@gmail.com>
2021-05-13 11:09:29 -0600

    Finished lidocaine analysis

--- code/lidocaine.R
+++ code/lidocaine.R
@@ -1,12 +1,24 @@
 library(tidyverse)
-source('code/get_dist.R')
+source('code/get_cd.R')
+source("zab_code/FullOriginalGmetaCode.R")
 # Lidocaine Dataset.csv: data
+# Lidocaine1.txt: data (long format)
+# Lidocaine_distn_1.txt: distribution
 
+#################### Data ####################
 data <- read_csv('data/Lidocaine Dataset.csv') %>%
-  select(-Study) %>%
-  pivot_longer(everything(), names_to = c('Treatment', '.value'), names_sep = '_') %>%
-  mutate(Treatment = as.numeric(Treatment == 'Lidocaine')) %>%
-  arrange(Treatment)
+  select(-Study)
+t_obs <- sum(data$Lidocaine_Dead)
 
-t_obs <- sum(data$Treatment*data$Dead)
-dist <- get_dist(data$Treatment, data$Dead, data$Total)
+dist <- read_table('data/Lidocaine_distn_1.txt', col_names = c('t', 'p', 'p.norm'))
+
+#################### Graph ####################
+tibble(
+  theta = seq(-3, 2, length = 1000),
+  perm = 1 - 2*abs(0.5-map_dbl(theta, get_cd, dist, t_obs)),
+  liu = 1 - 2*abs(0.5-gmeta(as.matrix(data), gmi.type = '2x2', method = 'exact1', gmo.xgrid = theta, report.error = TRUE)$combined.cd)
+) %>%
+  pivot_longer(-theta, names_to = 'method', values_to = 'alpha') %>%
+  ggplot(aes(theta, alpha, col = method)) +
+  geom_line() +
+  geom_hline(yintercept = 0.05, linetype = 'dashed')
