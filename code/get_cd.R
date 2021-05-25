9c1b354ce483f9304b456aa342abfae784cce2bf
Travis Andersen <andert714@gmail.com>
2021-05-25 14:36:41 -0600

    Vectorized get_cd

--- code/get_cd.R
+++ code/get_cd.R
@@ -1,11 +1,11 @@
-get_cd <- function(beta, dist, t){
-  # Prepare variables
-  prob <- dist$p.norm
-  u <- dist$t
-  t <- t - min(u)
+get_cd <- function(thetas, prob, u, t_obs){
+  # rescale test statistic
+  t_obs <- t_obs - min(u)
   u <- u - min(u)
-  # Calculate CD
-  u_prob <- prob*exp(u*beta)
-  p <- u_prob/sum(u_prob)
-  sum(p[u > t]) + 0.5*p[u == t]
+  # get normalized probabilites for each theta
+  P_unnorm <- prob*exp(u %x% t(thetas))
+  P_norm <- t(t(P_unnorm)*colSums(P_unnorm)^(-1))
+  # sum up extreme values
+  extreme <- as.numeric(u > t_obs) + 0.5*(u == t_obs)
+  colSums(P_norm*extreme)
 }
