b428057b52c7e150f772d2086d8b816fcef14c8d
Travis Andersen <andert714@gmail.com>
2021-05-25 14:37:04 -0600

    Graphed 20 simulations with known theta

--- code/sim.R
+++ code/sim.R
@@ -1,38 +1,36 @@
 library(tidyverse)
 source('zab_code/pRandom_flexible.R')
-source('code/get_dist.R')
 source('code/get_cd.R')
-source('code/get_est.R')
 set.seed(1)
 
-sim <- pRandom(
-  theta = 1, 
-  k = 5, 
-  p_ic_init = 0.1, 
-  tau2 = 0.2, 
-  min.n = 20, 
-  max.n = 30
-) %>%
-  as_tibble
+sims <- rerun(20, {
+  invalid <- TRUE
+  while(invalid){
+    sim <- pRandom(
+      theta = 1, 
+      k = 5, 
+      p_ic_init = 0.1, 
+      tau2 = 0.2, 
+      min.n = 20, 
+      max.n = 30
+    ) %>%
+      as_tibble
 
-sim_long <- sim %>%
-  pivot_longer(everything(), names_to = c('Treatment', '.value'), names_sep = '_') %>%
-  mutate(Treatment = as.numeric(Treatment == 'TRT')) %>%
-  arrange(Treatment)
-x <- sim_long$Treatment
-z <- sim_long$event
-n <- sim_long$n
+    dist <- with(sim, rema::rema(TRT_event, TRT_n, CTRL_event, CTRL_n))
+    t_obs <- sum(sim$TRT_event)
+    invalid <- t_obs %in% range(dist$test.stat)
 
-t_obs <- sum(x*z)
-dist <- get_dist(x, z, n)
-dist
-t_obs
+    cd <- tibble(
+      thetas =  seq(-2, 4, length = 1000),
+      cd = with(dist, get_cd(thetas, norm.probs, test.stat, t_obs))
+    )
+  }
+  cd
+})
 
-tibble(
-  theta = seq(-10, 10, length = 1000),
-  cd = map_dbl(theta, get_cd, dist, 21),
-  pval_func = 1 - 2*abs(0.5-cd)
-) %>%
-  ggplot() +
-  geom_line(aes(theta, pval_func))
+sim_df <- bind_rows(sims, .id = 'simulation') %>%
+  mutate_at('simulation', as.numeric)
 
+ggplot(sim_df, aes(thetas, 1 - 2*abs(0.5-cd))) +
+  geom_line() +
+  facet_wrap(~simulation, nrow = 4, ncol = 5)
