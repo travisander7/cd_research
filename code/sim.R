b7601a33d81e928417206cb18c4fb1216e2801f9
Travis Andersen <andert714@gmail.com>
2021-06-02 14:41:27 -0600

    Density, distribution, and curve plots with liu

--- code/sim.R
+++ code/sim.R
@@ -1,36 +1,68 @@
 library(tidyverse)
 source('zab_code/pRandom_flexible.R')
 source('code/get_cd.R')
-set.seed(1)
+source("zab_code/FullOriginalGmetaCode.R")
+# 4 middle, 1 high end, 55 low end
+seed <- 4
+set.seed(seed)
 
-sims <- rerun(20, {
-  invalid <- TRUE
-  while(invalid){
-    sim <- pRandom(
-      theta = 1, 
-      k = 5, 
-      p_ic_init = 0.1, 
-      tau2 = 0.2, 
-      min.n = 20, 
-      max.n = 30
-    ) %>%
-      as_tibble
+data <- pRandom(
+  theta = 1, 
+  k = 5, 
+  p_ic_init = 0.1, 
+  tau2 = 0.2, 
+  min.n = 20, 
+  max.n = 30
+) %>%
+  as_tibble %>%
+  select(TRT_event, TRT_n, CTRL_event, CTRL_n)
 
-    dist <- with(sim, rema::rema(TRT_event, TRT_n, CTRL_event, CTRL_n))
-    t_obs <- sum(sim$TRT_event)
-    invalid <- t_obs %in% range(dist$test.stat)
+rema_obj <- with(data, rema::rema(TRT_event, TRT_n, CTRL_event, CTRL_n, alpha = 0.05))
+dist <- rema_obj$dist
+t_obs <- sum(data$TRT_event)
 
-    cd <- tibble(
-      thetas =  seq(-2, 4, length = 1000),
-      cd = with(dist, get_cd(thetas, norm.probs, test.stat, t_obs))
-    )
-  }
-  cd
-})
-
-sim_df <- bind_rows(sims, .id = 'simulation') %>%
-  mutate_at('simulation', as.numeric)
-
-ggplot(sim_df, aes(thetas, 1 - 2*abs(0.5-cd))) +
+tibble(
+  thetas = seq(-2, 4, length = 1000),
+  cd = get_cd(thetas, dist$norm.probs, dist$test.stat, t_obs),
+  liu = gmeta(
+    as.matrix(data), 
+    gmi.type = '2x2', 
+    method = 'exact1', 
+    gmo.xgrid = thetas, 
+    report.error = TRUE
+  )$combined.cd
+) %>%
+  pivot_longer(-thetas, names_to = 'method', values_to = 'distribution') %>%
+  group_by(method) %>%
+  mutate(
+    curve = 1 - 2*abs(0.5-distribution),
+    density = c(NA, diff(distribution, 2)/diff(thetas, 2), NA)
+  ) %>%
+  pivot_longer(-c(thetas, method), names_to = 'type') %>%
+  ggplot(aes(thetas, value, col = method)) +
   geom_line() +
-  facet_wrap(~simulation, nrow = 4, ncol = 5)
+  facet_wrap(~type, ncol = 1)
+ggsave(str_c('data/first', seed, '.png'))
+
+ci_df <- seq(1e-3, 1-1e-3, length = 100) %>%
+  set_names %>%
+  map_df(
+    function(alpha){
+      ci <- with(data, rema::rema(TRT_event, TRT_n, CTRL_event, CTRL_n, alpha = alpha)$CI) %>%
+        t
+      colnames(ci) <- c('lower', 'upper')
+      as_tibble(ci)
+    }, .id = 'alpha'
+  ) %>%
+  mutate_at('alpha', as.numeric)
+
+tibble(
+  theta = seq(-2, 5, length = 1000),
+  cd = get_cd(theta, dist$norm.probs, dist$test.stat, t_obs)
+) %>%
+  ggplot() +
+  geom_line(aes(lower, alpha), ci_df) +
+  geom_line(aes(upper, alpha), ci_df) +
+  geom_line(aes(exp(theta), 1-2*abs(0.5-cd)), col = 'blue', linetype = 'dashed') +
+  xlim(0, 50)
+ggsave(str_c('data/second', seed, '.png'))
