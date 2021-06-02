894bb647e29a399b1b13ef92bbea2d94fb306d2d
Travis Andersen <andert714@gmail.com>
2021-06-02 10:03:01 -0600

    Redid get_est without tibble input

--- code/get_est.R
+++ code/get_est.R
@@ -1,19 +1,16 @@
 library(tidyverse)
 source('zab_code/CMLE.R')
 
-get_est <- function(dist, t, alpha = 0.05){
-  u <- dist$t
-  p <- dist$p.norm
+get_est <- function(prob, u, t_obs, alpha = 0.05){
+  obs_prob <- prob[u == t_obs]
+  p_val <- sum(prob[prob <= obs_prob])
+  midp_val <- sum(prob[prob < obs_prob]) + 0.5*obs_prob
 
-  t_prob <- p[u == t]
-  p_val <- sum(p[p <= t_prob])
-  midp_val <- sum(p[p < t_prob]) + 0.5*t_prob
-
-  p <- ifelse(p == 0, 1.0e-16, p)
+  prob <- ifelse(prob == 0, 1.0e-16, prob)
 
   only_one <- length(u) == 1
-  at_extreme <- t %in% range(u)
-  approx_extreme <- min(sum(p[u < t]), sum(p[u > t])) < 1.0e-15
+  at_extreme <- t_obs %in% range(u)
+  approx_extreme <- min(sum(prob[u < t_obs]), sum(prob[u > t_obs])) < 1.0e-15
 
   if(only_one){
     print('Error: Only one in distribution')
@@ -22,13 +19,13 @@ get_est <- function(dist, t, alpha = 0.05){
   } else if(approx_extreme){
     print('Error: Approximately at extreme')
   } else {
-    t <- t - min(u)
+    t_obs <- t_obs - min(u)
     u <- u - min(u)
-    beta_hats <- CMLE(p, u, t, t.or.alpha = t, ci = FALSE)
-    lower <- CMLE(p, u, t, t.or.alpha = alpha/2, ci = TRUE, dir = 'lower', midp = FALSE)
-    upper <- CMLE(p, u, t, t.or.alpha = alpha/2, ci = TRUE, dir = 'upper', midp = FALSE)
-    midp_lower <- CMLE(p, u, t, t.or.alpha = alpha/2, ci = TRUE, dir = 'lower', midp = TRUE)
-    midp_upper <- CMLE(p, u, t, t.or.alpha = alpha/2, ci = TRUE, dir = 'upper', midp = TRUE) 
+    beta_hats <- CMLE(prob, u, t_obs, t.or.alpha = t_obs, ci = FALSE)
+    lower <- CMLE(prob, u, t_obs, t.or.alpha = alpha/2, ci = TRUE, dir = 'lower', midp = FALSE)
+    upper <- CMLE(prob, u, t_obs, t.or.alpha = alpha/2, ci = TRUE, dir = 'upper', midp = FALSE)
+    midp_lower <- CMLE(prob, u, t_obs, t.or.alpha = alpha/2, ci = TRUE, dir = 'lower', midp = TRUE)
+    midp_upper <- CMLE(prob, u, t_obs, t.or.alpha = alpha/2, ci = TRUE, dir = 'upper', midp = TRUE) 
     tibble(beta_hats, lower, upper, midp_lower, midp_upper, p_val, midp_val, only_one, at_extreme, approx_extreme) 
   }
 }
