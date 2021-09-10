aaa7fb2029ed2d682c1fae477d81b007983ebdad
Travis Andersen <andert714@gmail.com>
2021-09-10 09:25:23 -0600

    Files for analyzing sim study

--- code/metrics.R
+++ code/metrics.R
@@ -7,18 +7,39 @@ theta <- 1
 sim <- pRandom(theta, 10, 0.05, 0.5, 20, 30)
 
 m <- with(sim, rema::rema(TRT_event, TRT_n, CTRL_event, CTRL_n))
-m$dist
-m$tstat
-
+p_value <- 1 - abs(1 - 2*get_cd(0,  m$dist$norm.probs, m$dist$test.stat, m$tstat))
 height <- 1 - abs(1 - 2*get_cd(theta,  m$dist$norm.probs, m$dist$test.stat, m$tstat))
 
-tibble(
+cd_df <- tibble(
   thetas = seq(-4, 4, length = 1000),
   dist = get_cd(thetas, m$dist$norm.probs, m$dist$test.stat, m$tstat),
-  curve = 1-abs(1-2*dist)
-) %>%
+  curve = 1-abs(1-2*dist),
+  density = c(NA, diff(dist, 2)/diff(thetas, 2), NA)
+)
+counternull <- cd_df %>%
+  filter(thetas < m$TE) %>%
+  summarize(thetas[which.min(abs(curve - p_value))]) %>%
+  as.numeric
+
+cd_df %>%
+  pivot_longer(-thetas) %>%
+  ggplot(aes(thetas, value)) +
+  geom_line() +
+  facet_wrap(~name, ncol = 1)
+ggsave('data/curve_density.png')
+
+cd_df %>%
+  ggplot() +
+  geom_line(aes(thetas, curve)) +
+  geom_point(aes(x = counternull, y = p_value), shape = 'circle open', size = 3) +
+  geom_hline(yintercept = 0.05, linetype = 'dashed') +
+  geom_vline(xintercept = 0)
+ggsave('data/cd.png')
+
+cd_df %>%
   ggplot(aes(thetas, curve)) +
   geom_area(alpha = 0.3, col = 'black', fill = 'blue') +
   geom_vline(xintercept = theta) +
   geom_hline(yintercept = height, linetype = 'dashed')
 ggsave('data/metric.png')
+
--- code/sim_results.R
+++ code/sim_results.R
@@ -16,21 +16,26 @@ df %>%
 	facet_grid(vars(tau2), vars(p_ic_init))
 
 # Analysis of area and height by theta, p_ic_init, and tau2
+
 plot <- df %>%
 	filter(!only_one, !extreme) %>%
 	group_by(theta, p_ic_init, tau2, method) %>%
 	summarize(
 		area = mean(area),
-		height = mean(height)
+		height = mean(height),
+		cover = mean(lower < theta & upper > theta)
 	) %>%
 	ggplot(aes(theta, col = method)) +
 	facet_grid(vars(tau2), vars(p_ic_init))
 
 plot + geom_line(aes(y = area))
+ggsave('data/area.png')
 plot + geom_line(aes(y = height))
+plot + geom_line(aes(y = cover))
+# y aesthetic is coverage (95%)
 
 # Analyze whether height ~ Unif(0, 1)
 df %>%
 	filter(!only_one, !extreme) %>%
 	ggplot(aes(height, col = method)) +
-	geom_density()
+	geom_density(adjust = 2)
